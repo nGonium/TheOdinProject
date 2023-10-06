60b6a800c12e49750fcdca7212b0d6e8c74eea53
Didier van Os <dvos@sogyo.nl>
2023-10-06 16:15:16 +0200

    Add new books through form
    Squashed commit of the following:
    
    commit d13a049117001609341b50fe6fd708e64c2f1871
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 16:07:30 2023 +0200
    
        Reset form after submit
    
    commit 90977b6b073fb44a6364ad43ad024d4c3851e704
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 16:05:16 2023 +0200
    
        Append new books
    
    commit be9a31142c95a469200ac9810f3911fc82f7a954
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 15:26:10 2023 +0200
    
        Toggle form visibility
    
    commit ffe5a20ed3615e67ac7b121cc06150e256780048
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 15:09:47 2023 +0200
    
        Create new book form

--- library/src/main.js
+++ library/src/main.js
@@ -26,12 +26,65 @@ function createBookElement(book) {
   return element
 }
 
-function renderBooksToPage() {
-  const BOOK_CONTAINER_ELEMENT = document.getElementById("book-container")
-  myLibrary.map(book => {
+const BOOK_CONTAINER_ELEMENT = document.getElementById("book-container")
+
+function appendBookToPage(book) {
     const element = createBookElement(book)
     BOOK_CONTAINER_ELEMENT.append(element)
-  })
+}
+
+function renderBooksToPage() {
+  myLibrary.map(appendBookToPage)
+}
+
+function handleOpenNewBookForm(event) {
+  event.preventDefault()
+
+  const form = document.getElementById("new-book-form")
+  form.classList.remove("hidden")
+
+  event.target.classList.add("hidden")
+}
+
+function handleCloseNewBookForm(event)  {
+  event.preventDefault()
+
+  const form = document.getElementById("new-book-form")
+  form.classList.add("hidden")
+
+  const openNewBookButton = document.getElementById("open-new-book-form-button")
+  openNewBookButton.classList.remove('hidden')
+}
+
+function handleNewBookFormSubmit(event) {
+  event.preventDefault()
+  const form = event.srcElement
+
+  const formData = new FormData(form)
+  const book = new Book(
+    formData.get('title'),
+    formData.get('author'),
+    formData.get('pages'),
+    formData.get('is-read'),
+  )
+
+  myLibrary.push(book)
+  appendBookToPage(book)
+
+  form.reset()
+}
+
+
+function addEventListeners() {
+  const openNewBookButton = document.getElementById("open-new-book-form-button")
+  openNewBookButton.addEventListener("click", handleOpenNewBookForm)
+
+  const closeNewBookFormButton = document.getElementById("close-new-book-form-button")
+  closeNewBookFormButton.addEventListener("click", handleCloseNewBookForm)
+
+  const newBookForm = document.getElementById("new-book-form")
+  newBookForm.addEventListener("submit", handleNewBookFormSubmit)
 }
 
 renderBooksToPage()
+addEventListeners()
