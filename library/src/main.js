db30605ff47f10a96677bbba41798ffcba1308ac
Didier van Os <dvos@sogyo.nl>
2023-10-06 14:41:21 +0200

    Create book view
    Squashed commit of the following:
    
    commit 247cc2adcabd5c1bba7696e80cb886305a321564
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 14:38:42 2023 +0200
    
        Style books
    
    commit 7298405757925e2683cc26297700ef0d634b6f3f
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 14:33:23 2023 +0200
    
        Contain books on page
    
    commit 8d55f5ce40a54babf7eb62696d32a675cb809681
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 14:26:10 2023 +0200
    
        Render books to page
    
    commit 0e3acba3e2536f8191118c8416c8e2d8d7475578
    Author: Didier van Os <dvos@sogyo.nl>
    Date:   Fri Oct 6 14:19:12 2023 +0200
    
        Stub book data

--- library/src/main.js
+++ library/src/main.js
@@ -1,4 +1,9 @@
-const myLibrary = [];
+const bookData = [
+  new Book("The Hobbit", "J.R.R. Tolkien", 310, true),
+  new Book("War and Peace", "L. Tolstoy", 1225, false),
+]
+
+const myLibrary = [...bookData];
 
 function Book(title, author, numPages, isRead) {
   this.title = title
@@ -13,3 +18,20 @@ function addBookToLibrary(title, author, numPages, isRead) {
   const newBook = new Book(title, author, numPages, isRead)
   myLibrary.push(newBook)
 }
+
+function createBookElement(book) {
+  const element = document.createElement('div')
+  element.classList.add('book-wrapper')
+  element.textContent = book.info()
+  return element
+}
+
+function renderBooksToPage() {
+  const BOOK_CONTAINER_ELEMENT = document.getElementById("book-container")
+  myLibrary.map(book => {
+    const element = createBookElement(book)
+    BOOK_CONTAINER_ELEMENT.append(element)
+  })
+}
+
+renderBooksToPage()
